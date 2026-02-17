df4e3e21a61af21f5efcada4c62a24bce6cf9680
Тина Исабек <tynasien@Mac.lan>
2026-02-17 23:40:47 +0500

    изменена реализация логики построения графа знаний на основе новых данных

--- src/knowledge_graph.py
+++ src/knowledge_graph.py
@@ -1,52 +1,33 @@
 import networkx as nx
-from models import Movie, Genre, Person
+from src.models import Movie
+from src.mock_data import movies_data
 
 def create_graph():
     G = nx.Graph()
-
-    # Фильмы
-    movie1 = Movie("Интерстеллар", genres=["Sci-Fi", "Drama"], directors=["Кристофер Нолан"], actors=["Мэттью МакКонахи"])
-    movie2 = Movie("Начало", genres=["Sci-Fi", "Action"], directors=["Кристофер Нолан"], actors=["Леонардо ДиКаприо"])
-    movie3 = Movie("Темный рыцарь", genres=["Action", "Crime"], directors=["Кристофер Нолан"], actors=["Кристиан Бэйл"])
     
-    movies = [movie1, movie2, movie3]
+    for data in movies_data:
+        movie = Movie(
+            name=data["title"],
+            year=data["year"],
+            genres=data["genres"],
+            rating=data["imdb_score"],
+            description=data["description"],
+            poster_url=data["poster"]
+        )
 
-    # жанры
-    all_genres = set()
-    for movie in movies:
-        all_genres.update(movie.genres)
-    for genre in all_genres:
-        G.add_node(genre, type="genre")
+        G.add_node(movie.name, type="movie", rating=movie.rating, year=movie.year)
 
-    # добавить людей
-    all_people = {}
-    for movie in movies:
-        for director in movie.directors:
-            all_people[director] = "Director"
-        for actor in movie.actors:
-            all_people[actor] = "Actor"
-    for person, role in all_people.items():
-        G.add_node(person, type=role.lower())
+        for g in movie.genres:
+            G.add_node(g, type="genre")
+            G.add_edge(movie.name, g)
 
-    # фильсы как узлы
-    for movie in movies:
-        G.add_node(movie.name, type="movie")
-
-    # связи
-    for movie in movies:
-        for genre in movie.genres:
-            G.add_edge(movie.name, genre)
-        for director in movie.directors:
-            G.add_edge(movie.name, director)
-        for actor in movie.actors:
-            G.add_edge(movie.name, actor)
-
+        if movie.year != "Unknown":
+            G.add_node(movie.year, type="year")
+            G.add_edge(movie.name, movie.year)
+            
     return G
 
-def find_related_entities(graph, start_node):
-    """
-    Найти все объекты, связанные с start_node
-    """
-    if start_node not in graph:
+def get_recommendations(G, node_name):
+    if node_name not in G:
         return []
-    return list(graph.neighbors(start_node))
+    return list(G.neighbors(node_name))
