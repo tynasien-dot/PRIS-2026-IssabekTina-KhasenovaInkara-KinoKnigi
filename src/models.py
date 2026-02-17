c45375e75ae57e626e1877d12308121a8a95be9a
Тина Исабек <tynasien@Mac.lan>
2026-02-17 23:38:27 +0500

    изменена разработка базовых моделей данных для фильмов и связей

--- src/models.py
+++ src/models.py
@@ -3,20 +3,12 @@ from typing import List
 
 @dataclass
 class Movie:
-    name: str                 # Название фильма
-    genres: List[str] = field(default_factory=list)   # Жанры
-    directors: List[str] = field(default_factory=list) # Режиссеры
-    actors: List[str] = field(default_factory=list)    # Актеры
-    rating: float = 0.0   
+    name: str
+    year: str
+    genres: List[str] = field(default_factory=list)
+    rating: float = 0.0
+    description: str = "" 
+    poster_url: str = ""  
 
     def __str__(self):
-        return f"{self.name} (Genres: {', '.join(self.genres)}, Directors: {', '.join(self.directors)}, Actors: {', '.join(self.actors)})"
-
-@dataclass
-class Genre:
-    name: str
-
-@dataclass
-class Person:
-    name: str
-    role: str 
+        return f"{self.name} ({self.year})"
